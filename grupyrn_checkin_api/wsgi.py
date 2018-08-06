fb7b53e147b3336f0cc0b7c1f107b98a98e6d470
Sedir Morais <philippi.sedir@gmail.com>
2018-08-06 08:43:19 -0300

    Package renamed

--- grupyrn_checkin_api/wsgi.py
+++ grupyrn_checkin_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for grupyrn_meetup project.
+WSGI config for grupyrn_checkin_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grupyrn_meetup.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grupyrn_checkin_api.settings")
 
 application = get_wsgi_application()
