e3923512a097b30b57fde94d19bc82e0c2eb15d7
Sedir Morais <philippi.sedir@gmail.com>
2019-01-14 12:56:17 -0300

    Fix registration view

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ import base64
 from datetime import date
 
 from django.contrib import messages
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.utils.datastructures import OrderedSet
 from django.utils.translation import gettext_lazy as _
@@ -41,19 +42,28 @@ class AttendeeRegistrationView(FormView):
     success_url = '/thanks/'
 
     def get(self, request, *args, **kwargs):
-        event = get_object_or_404(Event, slug=kwargs['event'], eventday__date__gte=date.today(),
-                                  closed_registration=False)
+        event = OrderedSet(Event.objects.filter(slug=kwargs['event'], eventday__date__gte=date.today(),
+                                                closed_registration=False))
+        if not len(event):
+            raise Http404
+
+        event = list(event)[0]
+
         return self.render_to_response(self.get_context_data(event=event))
 
     def form_valid(self, form):
-        event = get_object_or_404(Event, slug=self.request.POST['event'], eventday__date__gte=date.today(),
-                                  closed_registration=False)
+        event = OrderedSet(Event.objects.filter(slug=self.request.POST['event'], eventday__date__gte=date.today(),
+                                                closed_registration=False))
+        if not len(event):
+            raise Http404
+
+        event = list(event)[0]
 
         attendee = form.instance
         attendee.event = event
 
         try:
-            qr_data = senders.send_registration_mail(attendee, event)
+            qr_data = senders.send_registration_mail(attendee, event[0])
             context = self.get_context_data(qr_code=base64.b64encode(qr_data).decode('ascii'))
             attendee.save()
 
