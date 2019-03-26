4a12bc0d0dc8c93aa54d9744eee9fafffd57dc65
Sedir Morais <philippi.sedir@gmail.com>
2019-03-26 14:47:24 -0300

    Check-in system Jararaquinha integrated as a git submodule.

--- predeploy.sh
+++ predeploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Packing React Check-in front end..."
-npm run webpack
+yarn build
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic -i node_modules -i src -i package.json -i public -i scripts -i *.lock --noinput
 
 echo "Compiling translations..."
 django-admin compilemessages -f -v 0
