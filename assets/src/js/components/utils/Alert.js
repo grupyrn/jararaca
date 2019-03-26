12ccf12536a6488675a7e9e00c506eecf0c924a9
Sedir Morais <philippi.sedir@gmail.com>
2019-03-26 14:44:29 -0300

    Remove check-in client from repo.

--- assets/src/js/components/utils/Alert.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Animation from "./Animation";
-
-const Alert = (props) => {
-    return <div className={"alert alert-" + props.type} role="alert">{props.children}</div>;
-};
-
-
-export default Alert;
