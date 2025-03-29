2b6ee0cf75198cf0ccb792d93cb6b0283c367e4b
Jhonata Medeiros <34169712+jhonatacaiob@users.noreply.github.com>
2025-03-29 02:33:42 -0300

    63 escrever testes unitários dos métodos da classe memeberinfo (#64)
    * Adiciona configurações do pytest no pyproject.toml
    
    * Adiciona testes para a classe memberinfo

--- jararaca/settings.py
+++ jararaca/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = "pt-BR"
 
 TIME_ZONE = 'America/Recife'
 
--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-from jararaca import settings as jararaca_settings
-
-
-def pytest_configure():
-    settings.configure(LANGUAGE_CODE='pt-BR')
